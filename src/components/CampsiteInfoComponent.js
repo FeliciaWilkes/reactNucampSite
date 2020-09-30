d07a4a54d6fac8ab5e78ecc4f050c39499e50dac
FeliciaWilkes <Feliciankc@gmail.com>
2020-09-29 20:24:41 -0500

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -19,7 +19,7 @@ function RenderCampsite({ campsite }) {
         </div>
     );
 }
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId }) {
     if (comments) {
         return (
             <div className="col-md m-1">
@@ -42,7 +42,7 @@ function RenderComments({ comments }) {
 
                     );
                 })}
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment} />
             </div>
         );
     }
@@ -67,6 +67,7 @@ class CommentForm extends Component {
     }
     handleSubmit(values) {
         this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
         console.log('Current State is: ' + JSON.stringify(values));
         alert('Current State is: ' + JSON.stringify(values));
     }
@@ -149,7 +150,11 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
+                    />
                 </div>
             </div>
         );
